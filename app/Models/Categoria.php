c6d86adeee9b4e65f1582ef186a9efd4b23e82ca
ianff03 <ian.flores.25@lacetania.cat>
2021-04-19 18:19:22 +0200

    categories

--- app/Models/Categoria.php
+++ app/Models/Categoria.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Categoria extends Model
 {
-    //
+    use HasFactory;
+    //taula
+    protected $table = 'categories';
 }
