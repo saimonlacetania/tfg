30919d730d98828785deabcb921eba877b6dd13e
abram7-lacetania <abraham.villar.88@lacetania.cat>
2021-04-19 18:13:53 +0200

    model botiga

--- app/Models/Botiga.php
+++ app/Models/Botiga.php
@@ -1,10 +1,18 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Botiga extends Model
 {
-    //
+    use HasFactory;
+        //taula
+        protected $table = 'botigues';
+
+        protected function user() {
+            return $this->belongsTo('App\Models\User', 'id_usuari');
+        }
 }
