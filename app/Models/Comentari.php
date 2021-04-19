064b3576e7d87ea583205481ac03b82fc4b00043
ianff03 <ian.flores.25@lacetania.cat>
2021-04-19 18:23:28 +0200

    comentaris

--- app/Models/Comentari.php
+++ app/Models/Comentari.php
@@ -2,9 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Comentari extends Model
 {
-    //
+    use HasFactory;
+
+    //taula
+    protected $table = 'comentaris';
+
+    //mètodes
+    //un comentari un usuari
+    public function user()
+    {
+        return $this->belongsTo("App\Models\User", "id_usuari");
+    }
+
+    //un comentari un producte
+    public function producte()
+    {
+        return $this->belongsTo("App\Models\User", "id_producte");
+    }
 }
