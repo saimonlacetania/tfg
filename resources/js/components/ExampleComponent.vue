ffc42b7aa83278a700989358cb952e4cc7481c5c
ianff03 <ian.flores.25@lacetania.cat>
2021-04-21 16:04:41 +0200

    adminlte

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -4,7 +4,20 @@
       <div class="title m-b-md">
         Mercat <small class="text-monospace">virtual</small>
       </div>
+      <div class="col-md-3 col-sm-6 col-xs-12">
+        <div class="info-box">
+          <span class="info-box-icon bg-aqua"
+            ><i class="ion ion-ios-gear-outline"></i
+          ></span>
 
+          <div class="info-box-content">
+            <span class="info-box-text">CPU Traffic</span>
+            <span class="info-box-number">90<small>%</small></span>
+          </div>
+          <!-- /.info-box-content -->
+        </div>
+        <!-- /.info-box -->
+      </div>
       <div class="links">
         <a href="https://laravel.com/docs">Docs</a>
         <a href="https://laracasts.com">Laracasts</a>
