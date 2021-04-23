b5fe2088cc472b5a0cae58f9214a64b6a07a2ce3
ianff03 <ian.flores.25@lacetania.cat>
2021-04-23 17:23:57 +0200

    auth

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,26 +7,21 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Mercat Virtual</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-    <link rel="stylesheet" href="../../../node_modules/admin-lte/dist/css/adminlte.min.css">
-    <link rel="stylesheet" href="../../../node_modules/admin-lte/plugins/fontawesome-free/css/all.min.css">
+
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-
-<body class="hold-transition sidebar-mini">
+<body>
     <div id="app">
-        @yield('content')
+            @yield('content')
     </div>
 </body>
-
-<!-- Scripts -->
-<script src="{{ asset('js/app.js') }}" defer></script>
-<script src="../../../node_modules/admin-lte/dist/js/adminlte.js"></script>
-<script src="../../../node_modules/admin-lte/plugins/chart.js/Chart.min.js"></script>
-
 </html>
